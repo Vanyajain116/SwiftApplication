fe7a214cc707cc478e0deb93ce412a855b1a4def
Vanyajain116 <98710574+Vanyajain116@users.noreply.github.com>
2022-02-26 21:48:32 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,34 @@
 
 import PackageDescription
 
+/// Represents a supported platform.
+struct SupportedPlatform {
+    static func macOS(_ version: MacOSVersion) -> SupportedPlatform
+    static func macOS(_ versionString: String) -> SupportedPlatform
+
+    static func iOS(_ version: IOSVersion) -> SupportedPlatform
+    static func iOS(_ versionString: String) -> SupportedPlatform
+}
+
+/// List of known versions.
+extension SupportedPlatform.MacOSVersion {
+    static let v10_11: MacOSVersion
+    static let v10_12: MacOSVersion
+    static let v10_13: MacOSVersion
+    static let v10_14: MacOSVersion
+    static let v10_15: MacOSVersion
+}
+
+final class Package {
+    init(
+        name: String,
+        platforms: [SupportedPlatform]? = nil,
+    )
+}
+
 let package = Package(
     name: "SwiftSample",
-    platforms: [.iOS(.v12), .macOS(.v10_15)],
+    platforms: [.iOS(.v11), .macOS(.v10_15)],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
