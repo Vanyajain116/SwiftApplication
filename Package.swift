b792e9cc45147717bcbb774e4239ff78ff0dc2dd
Vanyajain116 <98710574+Vanyajain116@users.noreply.github.com>
2022-02-26 21:24:26 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
+    platforms: [
+       .macOS(.v10_15), .iOS(.v14),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
